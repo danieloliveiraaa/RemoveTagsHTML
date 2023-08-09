63fd70607834b0c3c3d59cd7476a7bfe76834f3f
Daniel Oliveira <silvaoliveiradaniel3@gmail.com>
2023-08-09 09:26:33 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RemoveTagsHTML
+{
+    class Program
+    {
+        //DANIEL OLIVEIRA
+        //teste
+        //recebendo um texto em HTML, removendo suas tags (<div> <a> por exemplo), mantendo o texto original.
+
+        static void Main(string[] args)
+        {
+            var htmlString = "Olá, tudo bem! <div></div>Você clicou no globinho? Ele é um link digital.<div></div>caso não consiga, me comunique para corrigir.";
+
+            string texto = RemoveHtmlTags(htmlString);
+
+            Console.WriteLine("TEXTO ORIGINAL:" + htmlString);
+            Console.WriteLine("TEXTO CORRIGIDO:" + texto);
+        }
+
+        public static string RemoveHtmlTags(string htmlString)
+        {
+            string tags = @"<[^>]+?>";
+
+            string texto = Regex.Replace(htmlString, tags, string.Empty);
+
+            return texto;
+        }
+    }
+}
